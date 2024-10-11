04f02ebe13aeb6cb1962e4ec07109a72e41e2e09
lai2-24 <lai2-24@itismeucci.com>
2024-10-11 09:23:12 +0200

    Esercizio completato

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,13 +1,29 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
         System.out.println("Il client è partito!");
         Socket s = new Socket("localhost", 3000);
         System.out.println("Il client si è collegato");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        Scanner scanner = new Scanner(System.in);
+        String stringaInviata = scanner.nextLine();
+        out.writeBytes(stringaInviata + "\n");
+
+        String stringRicevuta = in.readLine();
+        System.out.println(stringRicevuta);
+
+        s.close();
     }
 }
